0e2c4f15f5437f67fc4c9161f3d2340111aa3013
Gabriel Osuobiem <osuobiem@gmail.com>
2020-02-24 01:46:35 +0100

    Add basic CRUD to test controller

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -6,6 +6,33 @@ let user = new UserModel();
 
 module.exports = {
   async get() {
-    return await user.get();
+    return await user.get({
+      id: {
+        $lt: 10
+      },
+      $ord: {
+        firstname: "ASC"
+      }
+    });
+  },
+
+  async create(data) {
+    user.attr = data;
+
+    return await user.add();
+  },
+
+  async update(data, id) {
+    user.attr = data;
+
+    return await user.update({
+      id
+    });
+  },
+
+  async delete(id) {
+    return await user.delete({
+      id
+    });
   }
 };
--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const User = require("./app/controllers/UserController");
 const app = require("express")();
 const http = require("http").createServer(app);
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.get("/users", (req, res) => {
   User.get()
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+app.get("/users/:id/delete", (req, res) => {
+  User.delete(req.params.id)
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+app.post("/users/new", (req, res) => {
+  User.create(req.body)
+    .then(resp => {
+      res.send(resp);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+app.post("/users/:id/update", (req, res) => {
+  User.update(req.body, req.params.id)
     .then(resp => {
       res.send(resp);
     })
